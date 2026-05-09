3d17b2bb67eff552263a95001b367177809f6311
pabokori <xbowsfren@gmail.com>
2026-05-09 17:46:56 +0800

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwIJdNv4G1PnF75HrpQXCtbKRySpFk-gAS64_ULPSfBVoYyrD4PH_KqPxQLXo_sawiP/exec"; 
 
 export interface FormField {
   id: string;
